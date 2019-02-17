b9ac6a3d817acd66f6c1c0e6e1b4c753fc384242
Michael Lazuardy <shawnmilke55@gmail.com>
2019-02-17 15:51:59 +0700

    Create prepaid tables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="#" class="dropdown-item">
+                                        Prepaid Balance
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
