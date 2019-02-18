0cfc9a80b808e42ea7c20d1495845cce883550bc
Michael Lazuardy <shawnmilke55@gmail.com>
2019-02-18 12:58:07 +0700

    fix helper

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -2,13 +2,11 @@
 
 
 if(!function_exists('ifError')){
-    function ifError($field,$class = null)
+    function ifError($errors,$field,$class = null)
     {
-        if(isset($errors)){
-            if($errors->has($field)){
-                $$say = sprintf('<span %s>%s</span>', $class != null ? "class=$class" : '', $errors->first($field));
-                return $say;
-            }
+        if ($errors->has($field)) {
+            $say = sprintf('<span %s>%s</span>', $class != null ? "class=$class" : '', $errors->first($field));
+            return $say;
         }
     }
 }
