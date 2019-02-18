cd1a4a82ece31e3fd653681f8ad967804f90046c
Michael Lazuardy <shawnmilke55@gmail.com>
2019-02-18 22:52:30 +0700

    create accessor to get rupiah value

--- app/Order.php
+++ app/Order.php
@@ -17,4 +17,13 @@ class Order extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    /**
+     * Accessor to get Rupiah
+     * so we dont need to render rupiah with number_format when needed
+     */
+    public function getRupiahAttribute()
+    {
+        return number_format($this->total, 0, ',', '.');
+    }
 }
