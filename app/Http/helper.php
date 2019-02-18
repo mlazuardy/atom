8662c0560140a393905900d08478c53432fa16fa
Michael Lazuardy <shawnmilke55@gmail.com>
2019-02-18 11:22:02 +0700

    helper for displaying error

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -1,2 +1,14 @@
 <?php
 
+
+if(!function_exists('ifError')){
+    function ifError($field,$class = null)
+    {
+        if(isset($errors)){
+            if($errors->has($field)){
+                $$say = sprintf('<span %s>%s</span>', $class != null ? "class=$class" : '', $errors->first($field));
+                return $say;
+            }
+        }
+    }
+}
